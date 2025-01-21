5844299ef0e0df16fecc7578f9dc04aae520b5d8
Falakhair <falak.shair.fs789@gmail.com>
2025-01-21 16:19:17 +0100

    commit  by falak mvn

--- src/test/java/org/apache/commons/codec/language/bm/RuleTest.java
+++ src/test/java/org/apache/commons/codec/language/bm/RuleTest.java
@@ -21,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-
 /**
  * Tests Rule.
  */
